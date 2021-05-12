71aaef0a1ee71725e1fc33f347f681a6ebd97ab7
23subbhashit <subbhashitmukherjee@gmail.com>
2021-05-12 11:55:44 +0530

    Updates

---
+++ app/src/main/java/www/bmsit/loginatuh/AuthMain.java
@@ -0,0 +1,14 @@
+package www.bmsit.loginatuh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_main);
+    }
+}
