43c1c347d2debff2ed6b0914ab2a73867a2d4634
23subbhashit <subbhashitmukherjee@gmail.com>
2021-05-12 17:38:05 +0530

    Updates

--- app/src/main/java/www/bmsit/loginatuh/MainActivity.java
+++ app/src/main/java/www/bmsit/loginatuh/MainActivity.java
@@ -1,15 +1,35 @@
 package www.bmsit.loginatuh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+
+import com.agrawalsuneet.dotsloader.loaders.LazyLoader;
+import com.agrawalsuneet.dotsloader.loaders.LightsLoader;
 
 public class MainActivity extends AppCompatActivity {
-
+    LazyLoader lights;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lights = (LazyLoader) findViewById(R.id.lazy);
+        LazyLoader loader = new LazyLoader(this, 30, 20, ContextCompat.getColor(this, R.color.loader_selected),
+                ContextCompat.getColor(this, R.color.loader_selected),
+                ContextCompat.getColor(this, R.color.loader_selected));
+        loader.setAnimDuration(500);
+        loader.setFirstDelayDuration(100);
+        loader.setSecondDelayDuration(200);
+        loader.setInterpolator(new LinearInterpolator());
+
+        lights.addView(loader);
+
+//        Intent i =new Intent(this,AuthMain.class);
+//        startActivity(i);
     }
+
 
 }
