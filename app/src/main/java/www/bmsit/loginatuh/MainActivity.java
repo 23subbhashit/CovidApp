ec4b51bb733c5cb44fb8df031466e53727a3cacf
TheGamerBoy13 <dholariashivang@gmail.com>
2021-05-12 19:52:24 +0530

    Added load screen animation

--- app/src/main/java/www/bmsit/loginatuh/MainActivity.java
+++ app/src/main/java/www/bmsit/loginatuh/MainActivity.java
@@ -17,16 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lights = (LazyLoader) findViewById(R.id.lazy);
-        LazyLoader loader = new LazyLoader(this, 30, 20, ContextCompat.getColor(this, R.color.loader_selected),
-                ContextCompat.getColor(this, R.color.loader_selected),
-                ContextCompat.getColor(this, R.color.loader_selected));
-        loader.setAnimDuration(500);
-        loader.setFirstDelayDuration(100);
-        loader.setSecondDelayDuration(200);
-        loader.setInterpolator(new LinearInterpolator());
-
-        lights.addView(loader);
+//        lights = (LazyLoader) findViewById(R.id.lazy);
+//        LazyLoader loader = new LazyLoader(this, 30, 20, ContextCompat.getColor(this, R.color.loader_selected),
+//                ContextCompat.getColor(this, R.color.loader_selected),
+//                ContextCompat.getColor(this, R.color.loader_selected));
+//        loader.setAnimDuration(1000);
+//        loader.setFirstDelayDuration(100);
+//        loader.setSecondDelayDuration(200);
+//        loader.setInterpolator(new LinearInterpolator());
+//
+//        lights.addView(loader);
 
 
         new Handler().postDelayed(new Runnable() {
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this,MainActivity2.class);
                 startActivity(i);
             }
-        }, 2000);
+        }, 5000);
 
 
     }
