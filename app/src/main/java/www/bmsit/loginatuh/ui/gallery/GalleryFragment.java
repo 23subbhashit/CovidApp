5f498e56d5efec25ac9c58d86b1725a0edf1db57
23subbhashit <subbhashitmukherjee@gmail.com>
2021-05-14 13:44:26 +0530

    Updates

--- app/src/main/java/www/bmsit/loginatuh/ui/gallery/GalleryFragment.java
+++ app/src/main/java/www/bmsit/loginatuh/ui/gallery/GalleryFragment.java
@@ -1,10 +1,13 @@
 package www.bmsit.loginatuh.ui.gallery;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +20,7 @@ import www.bmsit.loginatuh.R;
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,5 +35,6 @@ public class GalleryFragment extends Fragment {
             }
         });
         return root;
+
     }
 }
