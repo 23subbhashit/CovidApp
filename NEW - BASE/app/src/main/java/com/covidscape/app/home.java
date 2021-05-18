26ff85910919293de8d51a78a56bedb183783dbd
23subbhashit <subbhashitmukherjee@gmail.com>
2021-05-18 15:42:48 +0530

    Updates

--- NEW - BASE/app/src/main/java/com/covidscape/app/home.java
+++ NEW - BASE/app/src/main/java/com/covidscape/app/home.java
@@ -3,24 +3,26 @@ package com.covidscape.app;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 //import androidx.navigation.ui.setupWithNavController;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class home extends AppCompatActivity {
 
-    private BottomNavigationView nav;
-    private FragmentContainerView nav_cont;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        nav = (BottomNavigationView) findViewById(R.id.nav);
-        nav_cont = findViewById(R.id.fragview);
-
-        //nav.setupWithNavController(nav_cont);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav);
+        NavController navController = Navigation.findNavController(this, R.id.fragview);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
 
     }
