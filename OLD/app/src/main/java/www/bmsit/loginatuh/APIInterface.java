8ffe76afe02f9409768e5ad352cf2d7f14de029c
23subbhashit <subbhashitmukherjee@gmail.com>
2021-05-25 07:49:28 +0530

    Updates

--- OLD/app/src/main/java/www/bmsit/loginatuh/APIInterface.java
+++ OLD/app/src/main/java/www/bmsit/loginatuh/APIInterface.java
@@ -3,7 +3,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 public interface APIInterface {
-
     @GET("everything")
     Call<ResponseModel> getLatestNews(@Query("q") String source,@Query("apiKey") String apiKey);
 }
